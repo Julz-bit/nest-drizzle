271f7176ce986ce3ee208aad5858cdbc5fb2f677
Julz-01 <caraanjulz101@gmail.com>
2024-10-03 13:32:43 +0800

    refactor(config): use configService

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
-  await app.listen(3001);
+  const configService = app.get(ConfigService);
+  await app.listen(+configService.get<number>('APP_PORT') || 3001);
 }
 bootstrap();
